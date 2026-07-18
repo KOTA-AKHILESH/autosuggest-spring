055406d6c981e009008bb0aaf1ac404551f4e8a4
KOTA-AKHILESH <25071a6798@vnrvjiet.in>
2026-07-18 12:21:47 +0530

    akhil toggel

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,39 @@
-// alert("hello from js")
-console.log("hello from js")
+console.log("hello from js");
+
+var users = [
+    {
+        name: "John Doe",
+        gender: "Male♂️",
+        country: "USA",
+        college: "Stanford University",
+        img: "john.png"
+    },
+    {
+        name: "Jane Doe",
+        gender: "Female♀️",
+        country: "Canada",
+        college: "University of Toronto",
+        img: "jane.png"
+    }, 
+    {
+        name: "Kota Akhilesh",
+        gender: "Male♂️",
+        country: "India",
+        college: "VNR VJIET",
+        img: "akhil.jpeg"
+    }
+];
+
+var id = 0;
+
+function toggle() {
+    console.log("Hii");
+
+    id = (id + 1) % users.length;
+
+    document.getElementById("user-image").src = users[id].img;
+    document.getElementById("user-name").innerHTML = users[id].name;
+    document.getElementById("user-gender").innerHTML = users[id].gender;
+    document.getElementById("user-country").innerHTML = users[id].country;
+    document.getElementById("user-college").innerHTML = users[id].college;
+}
